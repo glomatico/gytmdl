b827cdf65ab1c33946dca22d1c9413ef9c9b8b36
R. M <50295204+glomatico@users.noreply.github.com>
2023-09-16 18:22:59 -0300

    create constants

---
+++ gytmdl/constants.py
@@ -0,0 +1,11 @@
+MP4_TAGS_MAP = {
+    "album": "\xa9alb",
+    "album_artist": "aART",
+    "artist": "\xa9ART",
+    "comment": "\xa9cmt",
+    "lyrics": "\xa9lyr",
+    "media_type": "stik",
+    "rating": "rtng",
+    "release_date": "\xa9day",
+    "title": "\xa9nam",
+}
