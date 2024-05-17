14c7ddd61cc3fdd21f9611c3357b353e1319ccf1
glomatico <smoraes.rafa@gmail.com>
2024-05-17 20:55:58 -0300

    Update constants.py

--- gytmdl/constants.py
+++ gytmdl/constants.py
@@ -11,12 +11,12 @@ MP4_TAGS_MAP = {
     "album": "\xa9alb",
     "album_artist": "aART",
     "artist": "\xa9ART",
-    "url": "\xa9url",
+    "date": "\xa9day",
     "lyrics": "\xa9lyr",
     "media_type": "stik",
     "rating": "rtng",
-    "date": "\xa9day",
     "title": "\xa9nam",
+    "url": "\xa9url",
 }
 
 X_NOT_FOUND_STRING = '{} not found at "{}"'
