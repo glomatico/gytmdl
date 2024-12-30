1262e7468fc42a8852be362d0a3cadaa673b5587
Rafael Moraes <50295204+glomatico@users.noreply.github.com>
2024-12-30 14:32:52 -0300

    set `player_client` to `mweb`

--- gytmdl/downloader.py
+++ gytmdl/downloader.py
@@ -74,7 +74,7 @@ class Downloader:
             "allowed_extractors": ["youtube", "youtube:tab"],
             "extractor_args": {
                 "youtube": {
-                    "player_client": ["android"],
+                    "player_client": ["mweb"],
                 },
             },
         }
